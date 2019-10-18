3e03f05eb564e32130ddf25c95358f977d3ce59e
Georgi Mavridis <georgimavridis21@gmail.com>
2019-10-18 14:32:52 +0300

    add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, thunkMiddleware)
+);
 
 export default store;
